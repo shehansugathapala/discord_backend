a217c7ee9c41cabc2db11d2e8ecfb24b4ece970e
shehansugathapala <92087505+shehansugathapala@users.noreply.github.com>
2024-05-25 11:23:00 +0530

    init

--- controllers/auth/authControllers.js
+++
@@ -1,7 +0,0 @@
-const postLogin = require('./postLogin');
-const postRegister = require('./postRegister');  // Correct relative path
-
-exports.controllers = {
-    postLogin,
-    postRegister,
-};
